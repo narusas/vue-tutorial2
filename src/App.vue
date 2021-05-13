6e22761ab45d527732bd6164483c84c4b951fc0d
Jisung Ahn <narusas@gmail.com>
2021-05-13 23:16:23 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-blue-400 w-full h-auto p-8 flex justify-center font-sans">
+    <tasks-panel></tasks-panel>
+  </div>
+</template>
+
+<script setup>
+import {Home, More} from '@icon-park/vue-next';
+
+import {ref} from "vue";
+import TasksPanel from "./components/TasksPanel.vue";
+
+const tasks  = ref({})
+
+</script>
+
---
+++ src/components/TasksPanel.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="rounded bg-blue-200 w-64 p-2">
+    <div class="flex justify-between py-1">
+      <h3 class="text-sm">New landing page</h3>
+      <more></more>
+    </div>
+    <div class="text-sm mt-2">
+      <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+        <input type="checkbox" class="rounded"/> Do a mobile first layout
+      </div>
+
+      <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+        <input type="checkbox" class="rounded" checked/> Check the meta tags
+      </div>
+
+      <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+        <input type="checkbox" class="rounded"/> Check the responsive layout on all devices
+
+      </div>
+      <p class="mt-3 text-grey-dark">Add a card...</p>
+    </div>
+  </div>
+
+</template>
+<script setup>
+
+</script>
